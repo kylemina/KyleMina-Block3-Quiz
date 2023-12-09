9969727815b810b51893c60c89e4032c9ad5bd26
kylemina <kysi.mina.up@phinmaed.com>
2023-12-09 23:55:12 +0800

    first commit

---
+++ app/src/main/java/com/example/kylemina_block3_quiz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kylemina_block3_quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
